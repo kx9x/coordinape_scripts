f9ec46df99ada59d0ac27714dc8087b3d76a6619
kx9x <kx9x@protonmail.com>
2021-05-05 12:32:41 -0700

    feat: add exclusion/funding methods, treasury swap (#6)
    * feat: add exclusion/funding methods, treasury swap
    
    * typo: Oni -> Orb

--- scripts/coordinape_disperse.py
+++ scripts/coordinape_disperse.py
@@ -10,7 +10,6 @@ from tabulate import tabulate
 
 EPOCH_RESULTS_ENDPOINT_FORMAT = "https://coordinape.me/api/{0}/csv?epoch={1}"
 
-
 class CoordinapeGroup(Enum):
     COMMUNITY = 1
     YSTRATEGIST = 2
@@ -19,6 +18,17 @@ class CoordinapeGroup(Enum):
     COORDINAPETESTING = 5
     CREAM = 6
     GITCOIN = 7
+
+
+class ExclusionMethod(Enum):
+    REDISTRIBUTE_SHARE = 1 # An excluded person's share is redistributed to the pool
+    REMOVE_SHARE = 2 # An excluded person's share is removed from the pool of funds
+
+
+class FundingMethod(Enum):
+    DEPOSIT_YFI = 1
+    TRANSFER_YVYFI = 2
+    TRANSFER_YVYFI_FROM_TREASURY = 3
 
 
 # For epoch 3 in the yearn community, we need to add the leftover USDC from epoch 2.
@@ -38,7 +48,7 @@ def contributors_from_epoch(group, epoch):
     return list(csv.DictReader(buff))
 
 
-def disperse(group, epoch, deposit_yfi, safe="ychad.eth"):
+def disperse(group, epoch, safe="ychad.eth", funding_method=FundingMethod.DEPOSIT_YFI, exclusion_list=[], exclusion_type=ExclusionMethod.REDISTRIBUTE_SHARE):
     assert (
         group in DEFAULT_USD_REWARD_DICT
     ), f"{group.name} does not have a default usd reward entry"
@@ -56,10 +66,18 @@ def disperse(group, epoch, deposit_yfi, safe="ychad.eth"):
         for contributor in contributors_from_epoch(group, epoch)
         if int(contributor["received"]) > 0
     ]
-    num_contributors = len(contributors_this_epoch)
+
+    # REDISTRIBUTE_SHARE means we will treat excluded folks as never being in the pool
+    # and their share will be distributed to others based on the other votes
+    if exclusion_type == ExclusionMethod.REDISTRIBUTE_SHARE:
+        contributors_this_epoch = [
+            contributor 
+            for contributor in contributors_this_epoch 
+            if contributor["address"] not in exclusion_list
+        ]
 
     assert (
-        num_contributors > 0
+        len(contributors_this_epoch) > 0
     ), "{group.name}'s epoch #{epoch} does not have any contributors with votes received..."
 
     total_votes = 0
@@ -80,15 +98,30 @@ def disperse(group, epoch, deposit_yfi, safe="ychad.eth"):
 
     yvyfi_before = yvyfi.balanceOf(safe.account)
     yfi_before = yfi.balanceOf(safe.account)
-    if deposit_yfi:
-        assert yfi.balanceOf(safe.account) >= yfi_allocated
+    yvyfi_to_disperse = Wei(
+        (yfi_allocated * yvyfi.totalSupply()) / yvyfi.totalAssets()
+    )
+
+    if funding_method == FundingMethod.DEPOSIT_YFI:
+        safe_yfi_balance = yfi.balanceOf(safe.account)
+        assert safe_yfi_balance >= yfi_allocated
         yfi.approve(yvyfi, yfi_allocated)
         yvyfi.deposit(yfi_allocated)
         yvyfi_to_disperse = Wei(yvyfi.balanceOf(safe.account) - yvyfi_before)
-    else:
-        yvyfi_to_disperse = Wei(
-            (yfi_allocated * yvyfi.totalSupply()) / yvyfi.totalAssets()
-        )
+
+        percentage_yvyfi_buffer = (yvyfi.balanceOf(safe.account) - yvyfi_to_disperse) / yvyfi_to_disperse
+        if percentage_yvyfi_buffer < 0.01:
+            print(f"Warning! This TX could fail if yvYFI's pricePerShare changes before execution.\nThe yvyfi buffer is only {percentage_yvyfi_buffer}%\n")
+            print(f"Press enter to continue or Ctrl-C to exit")
+            input()
+    elif funding_method == FundingMethod.TRANSFER_YVYFI_FROM_TREASURY:
+        treasury = safe.contract("0x93A62dA5a14C80f265DAbC077fCEE437B1a0Efde")
+        assert treasury.governance() == safe.account
+        assert yvyfi.balanceOf(treasury) >= yvyfi_to_disperse
+        treasury_yvyfi_before = yvyfi.balanceOf(treasury)
+        treasury.toGovernance(yvyfi, yvyfi_to_disperse)
+
+    if funding_method == FundingMethod.TRANSFER_YVYFI or FundingMethod.TRANSFER_YVYFI_FROM_TREASURY:
         assert yvyfi.balanceOf(safe.account) >= yvyfi_to_disperse
 
     # Converting here will leave some dust
@@ -100,9 +133,23 @@ def disperse(group, epoch, deposit_yfi, safe="ychad.eth"):
         for contributor in contributors_this_epoch
     ]
 
+    # REMOVE_SHARE means we will remove the excluded
+    # contributors and not distribute their share. Subtract their
+    # amount from the yvyfi to disperse.
+    yvyfi_removed_by_exclusion = 0
+    if exclusion_type == ExclusionMethod.REMOVE_SHARE:
+        for contributor_address in exclusion_list:
+            contributor_to_exclude = next(x for x in contributors_this_epoch if x["address"] == contributor_address)
+            index = contributors_this_epoch.index(contributor_to_exclude)
+            del contributors_this_epoch[index]
+            yvyfi_removed_by_exclusion += amounts[index]
+            yvyfi_to_disperse -= amounts[index]
+            yfi_allocated -= amounts[index] * (yvyfi.totalAssets() / yvyfi.totalSupply())
+            del amounts[index]
+
     # Dust should be less than or equal to 1 Wei per contributor due to the previous floor
     dust = yvyfi_to_disperse - sum(amounts)
-    assert dust <= num_contributors
+    assert dust <= len(contributors_this_epoch)
 
     # Some lucky folks can get some dust, woot
     for i in range(math.floor(dust)):
@@ -112,6 +159,9 @@ def disperse(group, epoch, deposit_yfi, safe="ychad.eth"):
     assert (
         yfi_allocated
         == yvyfi_to_disperse * (yvyfi.totalAssets() / yvyfi.totalSupply())
+        or
+        yfi_allocated
+        == yvyfi_to_disperse * yvyfi.pricePerShare() / yfi_decimal_multiplicand
     )
 
     yvyfi.approve(disperse, sum(amounts))
@@ -121,14 +171,19 @@ def disperse(group, epoch, deposit_yfi, safe="ychad.eth"):
     disperse.disperseToken(yvyfi, recipients, amounts)
     history[-1].info()
 
-    if deposit_yfi:
+    if funding_method == FundingMethod.DEPOSIT_YFI:
         # Make sure we sent all the new yvYFI and only used as much YFI as expected
         assert yvyfi_before == yvyfi.balanceOf(safe.account)
         assert yfi_before - yfi_allocated == yfi.balanceOf(safe.account)
-    else:
+    elif funding_method == FundingMethod.TRANSFER_YVYFI:
         # Make sure we didn't use YFI for some reason and only used as much yvYFI as expected
         assert yfi_before == yfi.balanceOf(safe.account)
         assert yvyfi_before - yvyfi_to_disperse == yvyfi.balanceOf(safe.account)
+    elif funding_method == FundingMethod.TRANSFER_YVYFI_FROM_TREASURY:
+        # Make sure we didn't use YFI and only used the yvYFI from the treasury
+        assert yfi_before == yfi.balanceOf(safe.account)
+        assert yvyfi_before + yvyfi_removed_by_exclusion == yvyfi.balanceOf(safe.account)
+        assert treasury_yvyfi_before - (yvyfi_to_disperse + yvyfi_removed_by_exclusion) == yvyfi.balanceOf(treasury)
 
     # For each recipient, make sure their yvYFI amount increased by the expected amount
     for recipient, yvyfi_before, amount in zip(
@@ -174,14 +229,16 @@ def disperse(group, epoch, deposit_yfi, safe="ychad.eth"):
     safe.post_transaction(safe_tx)
 
 
-def disperse_yearn_community_epoch_three():
-    disperse(CoordinapeGroup.COMMUNITY, 3, True)
+def disperse_yearn_community_epoch_3():
+    # Exclude Orb, redistribute his share
+    # Transfer yvyfi from treasury
+    disperse(CoordinapeGroup.COMMUNITY, 3, "ychad.eth", FundingMethod.TRANSFER_YVYFI_FROM_TREASURY, ["0x710295b5f326c2e47e6dd2e7f6b5b0f7c5ac2f24"], ExclusionMethod.REDISTRIBUTE_SHARE)
 
 
-def disperse_strategist():
-    disperse(CoordinapeGroup.YSTRATEGIST, 2, False, "brain.ychad.eth")
+def disperse_strategist_2():
+    disperse(CoordinapeGroup.YSTRATEGIST, 2, "brain.ychad.eth", FundingMethod.TRANSFER_YVYFI)
 
 
 if __name__ == "__main__":
     network.connect("mainnet-fork")
-    disperse_yearn_community_epoch_three()
+    disperse_yearn_community_epoch_3()
