d57ea419a90b96dd073bc44f0efa7cd55eb697d5
Jesse Liston <listonjesse@yahoo.com>
2021-05-15 09:50:26 -0700

    refactor: use more files, use more methods

---
+++ scripts/coordinape_enums.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class CoordinapeGroup(Enum):
+    COMMUNITY = 1
+    YSTRATEGIST = 2
+    _003 = 3
+    SUSHI = 4
+    COORDINAPETESTING = 5
+    CREAM = 6
+    GITCOIN = 7
+    BERKSHIRE_HATHAWAY = 8
+    WUFFY = 9
+
+
+class ExclusionMethod(Enum):
+    REDISTRIBUTE_SHARE = 1  # An excluded person's share is redistributed to the pool
+    REMOVE_SHARE = 2  # An excluded person's share is removed from the pool of funds
+
+
+class FundingMethod(Enum):
+    DEPOSIT_YFI = 1
+    TRANSFER_YVYFI = 2
+    TRANSFER_YVYFI_FROM_TREASURY = 3
