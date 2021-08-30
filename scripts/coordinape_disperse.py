e9451d140fb8a223935c3c75d4f19b176ea12bc3
kx9x <kx9x@protonmail.com>
2021-08-30 15:51:42 -0700

    fix: whoops

--- scripts/coordinape_disperse.py
+++ scripts/coordinape_disperse.py
@@ -176,11 +176,11 @@ def disperse_strategist_4():
 
 
 def disperse_strategist_5():
-    disperse(CoordinapeGroup.YSTRATEGIST, 6, BRAIN_YCHAD_ETH, FundingMethod.TRANSFER_YVYFI)
+    disperse(CoordinapeGroup.YSTRATEGIST, 5, BRAIN_YCHAD_ETH, FundingMethod.TRANSFER_YVYFI)
 
 
 def disperse_strategist_6():
-    disperse(CoordinapeGroup.YSTRATEGIST, 5, BRAIN_YCHAD_ETH, FundingMethod.TRANSFER_YVYFI)
+    disperse(CoordinapeGroup.YSTRATEGIST, 6, BRAIN_YCHAD_ETH, FundingMethod.TRANSFER_YVYFI)
 
 if __name__ == "__main__":
     network.connect("mainnet-fork")
